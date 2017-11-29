2e45a98a74fa1aeee5ae7ee982929f76ad6a7611
Patrick <pgreher@gmx.de>
2017-11-29 11:55:08 +0100

    Exercise 7 ohne Erklärung für diplayfft2

--- assignment_03/my_idealLowPassFilter.m
+++ assignment_03/my_idealLowPassFilter.m
@@ -2,7 +2,38 @@ function [ lpimg ] = my_idealLowPassFilter( img,maxFrequency )
 %MY_IDEALLOWPASSFFT Summary of this function goes here
 %   Detailed explanation goes here
     
-    lpimg = zeros(size(img));
+    %Fourier Transformation
+    ft = fft2(img);
+    
+    %create Ideal Low-Pass Filter
+    % 1 if sqrt(u^2+v^2) <= maxFrequency , Rest zero
+    [width, height] = size(img);
+    y=0:(width-1);
+    x=0:(height-1);
+    j=1;
+    
+   
+    for i=height/2:height
+        if x(i)>0
+            indices_x(:,j) = i;
+            j=j+1;
+        end
+    end
+    j=1;
+    for i=width/2:width
+        if y(i)>0
+            indices_y(:,j)= i;
+            j=j+1;
+        end
+    end
+    
+    x(indices_x)=x(indices_x)-height;
+    y(indices_y)=y(indices_y)-width;
+    [X,Y]=meshgrid(x,y);
+    filter=sqrt(X.^2+Y.^2)<=maxFrequency;
 
+    %Multiply Filter with Fourier Transfrom and create
+    %inverseFourierTransfrom to get the LowPass Image
+    lpimg = ifft2(filter.*ft);
 end
 
--- assignment_03/my_plotFFT2D.m
+++ assignment_03/my_plotFFT2D.m
@@ -2,6 +2,15 @@ function my_plotFFT2D( img , figName)
 %MY_PLOTFFT2D Summary of this function goes here
 %   Detailed explanation goes here
 
+%{
+    displayfft2 creates a diagram which shows 
+%}
+
+figure('name', figName);
+subplot(1,2,1);
+imshow(img);
+subplot(1,2,2);
+displayfft2(img);
 
 end
 
